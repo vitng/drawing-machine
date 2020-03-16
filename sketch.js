ad8cab159b9c8736e447b12721f899913e70c528
vitng <vi.ng0204@gmail.com>
2020-03-15 21:57:25 -0700

    added

--- sketch.js
+++ sketch.js
@@ -1,7 +1,45 @@
+let array = [];
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(2000,900);
+  background(255);
+  strokeWeight(5);
+  noFill();
 }
 
 function draw() {
-  background(220);
+
+  if (mouseIsPressed) {
+    //change stroke color (random?)
+    //stroke (map ( mouseX, 0, 600, 0, 255, true));
+    //reversed stroke
+    //line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+    //line(mouseX, mouseY, pmouseX, pmouseY);
+    background (0);
+    array.push([mouseX, mouseY]);
+
+  }
+
+}
+
+function keyTyped() {
+
+  if (key === "s") {
+    // save this image
+    saveCanvas('fileName', 'png');
+  } else if (key === 'd') {
+    // display image
+    background (255);
+
+    beginShape();
+    for (let i = 0; i < array.length; i++) {
+      //--->basic drawing for (let i=0; i< array.length-1; i++){
+      //line (array [i][0], array[i][1], array[i+1][0], array[i+1][1]);<-----
+      curveVertex(array[i][0], array[i][1])
+
+  }
+  endShape();
+}
+  return false;
+
 }
